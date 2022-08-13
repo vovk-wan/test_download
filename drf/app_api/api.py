0dba620305883f5586c1ccfff14d8b457d1c053a
Владимр Леонтьев <dllreg.exe@gmail.com>
2022-08-14 05:47:47 +0800

    version 1.0 edit api.py CsvProcessingView

--- drf/app_api/api.py
+++ drf/app_api/api.py
@@ -55,7 +55,7 @@ class CsvProcessingBoto3View(GenericAPIView, SerializedData):
         return Response(task, status=status.HTTP_201_CREATED)
 
 
-class CsvProcessingView(GenericAPIView):
+class CsvProcessingView(GenericAPIView, SerializedData):
     """
     Размещаем новое задание.
     Нужно отправить пару логин - пароль для авторизации в системе
@@ -64,42 +64,18 @@ class CsvProcessingView(GenericAPIView):
     serializer_class = GetloclaAndUrlTaskSerializer
 
     def post(self, request, *args, **kwargs):
+        data = self.get_data(request=request)
 
-        request_data = request.body.decode('utf8')
-        logger.info(
-            f'{self.__class__.__qualname__}, '
-            f'before json request_data: {request_data}'
+        user = self.authorization(
+            username=data['username'], password=data['password'])
+
+        task_id = processing_url_file(
+            file_name=data['file_name'],
+            file_url=data['file_url'],
+            user=user
         )
-        try:
-            data = json.loads(request_data)
-        except (AttributeError, json.decoder.JSONDecodeError) as err:
-            logging.error(err)
-            return JsonResponse(
-                {'result': 'fail'}, status=status.HTTP_400_BAD_REQUEST
-            )
-        username: str = data.get('username')
-        password: str = data.get('password')
-        user = authenticate(username=username, password=password)
-        if user is None:
-            return JsonResponse(
-                {'result': 'failed authorization'},
-                status=status.HTTP_403_FORBIDDEN
-            )
-        file_name: str = data.get('file_name')
-        file_url: str = data.get('file_url')
-        result = []
-        if file_url:
-            task_pk = Task.objects.create(user=user)
-            task = for_url_file_process.delay(
-                                            task_pk=task_pk.pk, url=file_url)
-            result.append(task.id)
-        if file_name:
-            task_pk = Task.objects.create(user=user)
-            task = local_file_process.delay(
-                                       task_pk=task_pk.pk, file_name=file_name)
-            result.append(task.id)
 
-        return JsonResponse({'task_id': result}, status=status.HTTP_200_OK)
+        return Response({'task_id': task_id}, status=status.HTTP_201_CREATED)
 
 
 class GetWorkingTasksView(GenericAPIView):
