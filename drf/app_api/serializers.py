f177139ffec51cf0fe58fb38a8ea4aef030a6ac8
Владимр Леонтьев <dllreg.exe@gmail.com>
2022-08-14 04:46:20 +0800

    version 1.0 edit serializers.py fix all serializers

--- drf/app_api/serializers.py
+++ drf/app_api/serializers.py
@@ -1,25 +1,40 @@
 from rest_framework import serializers
-from django.conf import settings
+
+
+class GetTaskResultSerializer(serializers.Serializer):
+    task_id = serializers.CharField(max_length=150)
+    username = serializers.CharField(max_length=50)
+    password = serializers.CharField(max_length=50)
+
+
+class GetCountTasktSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=50)
+    password = serializers.CharField(max_length=50)
 
 
 class GetloclaAndUrlTaskSerializer(serializers.Serializer):
-    file_name = serializers.FilePathField(path=settings.BASE_DIR)
-    file_url = serializers.URLField()
+    file_url = serializers.URLField(required=False, allow_blank=True)
+    file_name = serializers.CharField(
+        max_length=250, required=False, allow_blank=True)
     username = serializers.CharField(max_length=50)
     password = serializers.CharField(max_length=50)
 
+    def validate(self, attrs):
+        if attrs.get('file_name') or attrs.get('file_url'):
+            return attrs
+        raise serializers.ValidationError(
+                                        detail='at least one of the fields '
+                                        'file name or file URL must be filled'
+                                        )
 
-class GetUrlTaskSerializer(serializers.Serializer):
-    file_url = serializers.URLField()
-    username = serializers.CharField(max_length=50)
-    password = serializers.CharField(max_length=50)
 
 class GetBoto3TaskSerializer(serializers.Serializer):
-    access_key = serializers.URLField()
-    secret_key = serializers.URLField()
-    region_name = serializers.URLField()
+    access_key = serializers.CharField(max_length=150)
+    secret_key = serializers.CharField(max_length=150)
+    region_name = serializers.CharField(max_length=50)
     endpoint_s3_url = serializers.URLField()
-    bucket_name = serializers.URLField()
-    file_name = serializers.URLField()
+    bucket_name = serializers.CharField(max_length=150)
+    file_name = serializers.CharField(max_length=250)
     username = serializers.CharField(max_length=50)
     password = serializers.CharField(max_length=50)
+
